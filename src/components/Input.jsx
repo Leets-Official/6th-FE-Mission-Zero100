9e3fb2664ed5b290cb6baeee09c04e07c6b95cbf
HyeWon <101498350+One-HyeWon@users.noreply.github.com>
2025-09-24 16:46:32 +0900

    Revert "Revert "[1주차] 이상미/[feat] TodoList_UI구현""

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input({placeholder = ""}){
+  return <input type = "text" placeholder={placeholder}/>
+}
+
+export default Input;
+
+
+
+// <input type="text" placeholder="할 일을 입력하세요"> (html)
+
