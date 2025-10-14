16a29742e2c6ea0968fd4a36fae57cf4c8641d46
baeys0619 <baeys2005@gmail.com>
2025-10-14 17:48:24 +0900

    design : input, button borderRadius(rounded-md) 삭제

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ function Button({ disabled, children, className }) {
     <button
       disabled={disabled}
 
-      className={`w-full py-2 px-4 rounded-md font-semibold text-white bg-black hover:bg-gray-800 transition-colors ${className}`}
+      className={`w-full py-2 px-4 font-semibold text-white bg-black hover:bg-gray-800 transition-colors ${className}`}
     >
       {children}
     </button>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,7 @@ const Input = ({ name, placeholder, className }) => {
     <input
       name={name}
       placeholder={placeholder}
-      className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black ${className}`}
+      className={`w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-black ${className}`}
     />
   );
 };
