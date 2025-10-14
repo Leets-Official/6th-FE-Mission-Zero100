ecdfd5a89eb26864c7ac0eccbd0bdc6b26175a3c
baeys0619 <baeys2005@gmail.com>
2025-10-14 17:25:21 +0900

    refactor : Category컴포넌트를 map으로 렌더링하도록 수정함

--- src/modules/Category.jsx
+++ src/modules/Category.jsx
@@ -5,6 +5,11 @@ const CategoryButton = ({ children, isSelected, onClick }) => {
   const baseStyle = 'py-2 px-8 font-semibold transition-all duration-300 min-w-28 border border-black';
   const selectedStyle = 'bg-slate-900 text-white';
   const notSelectedStyle = 'bg-white text-slate-500 hover:bg-slate-100';
+  const CATEGORIES = [
+    {value : 'all', label: 'All'},
+    {value : 'active', label: 'Avtive'},
+    {value : 'completed', label: 'Completed'},
+  ];
 
   return (
     <button 
@@ -19,25 +24,16 @@ const CategoryButton = ({ children, isSelected, onClick }) => {
 
 function Category({ selected, onCategoryChange }) {
   return (
-    <div className="grid grid-cols-3 gap-2">
-      <CategoryButton 
-        isSelected={selected === 'All'} 
-        onClick={() => onCategoryChange('All')}
-      >
-        All
-      </CategoryButton>
-      <CategoryButton 
-        isSelected={selected === 'Active'}
-        onClick={() => onCategoryChange('Active')}
-      >
-        Active
-      </CategoryButton>
-      <CategoryButton 
-        isSelected={selected === 'Completed'}
-        onClick={() => onCategoryChange('Completed')}
-      >
-        Completed
-      </CategoryButton>
+    <div className="grid grid-cols gap-2">
+      {CATEGORIES.map(({value, label}) =>(
+        <CategoryButton
+        key={value}
+        isSelected={selected === value}
+        onClick={()=>onCategoryChange(value)}
+        >
+          {label}
+        </CategoryButton>
+      ))}
     </div>
   );
 }
