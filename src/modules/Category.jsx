bbba8ca0da9ab30bd50083a0486e799d5e66683d
baeys0619 <baeys2005@gmail.com>
2025-10-14 17:33:47 +0900

    fix : Uncaught ReferenceError: CATEGORIES is not defined 에러 수정

--- src/modules/Category.jsx
+++ src/modules/Category.jsx
@@ -1,15 +1,15 @@
-
+const CATEGORIES = [
+    {value : 'all', label: 'All'},
+    {value : 'active', label: 'Avtive'},
+    {value : 'completed', label: 'Completed'},
+  ];
 
 
 const CategoryButton = ({ children, isSelected, onClick }) => {
   const baseStyle = 'py-2 px-8 font-semibold transition-all duration-300 min-w-28 border border-black';
   const selectedStyle = 'bg-slate-900 text-white';
   const notSelectedStyle = 'bg-white text-slate-500 hover:bg-slate-100';
-  const CATEGORIES = [
-    {value : 'all', label: 'All'},
-    {value : 'active', label: 'Avtive'},
-    {value : 'completed', label: 'Completed'},
-  ];
+  
 
   return (
     <button 
