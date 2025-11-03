d73461c21d9d63d25b905af19b46374924fd10ae
baeys0619 <baeys2005@gmail.com>
2025-11-03 21:51:39 +0900

    feat : 회원가입 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,98 @@
-import { useState } from 'react';
-import Header from './modules/header';
-import AddTodo from './modules/addTodo';
-import Category from './modules/category';
-import TodoList from './modules/TodoList'; 
-import { useEffect } from 'react';
+// import { useState } from 'react';
+// import Header from './modules/Header';
+// import AddTodo from './modules/AddTodo';
+// import Category from './modules/Category';
+// import TodoList from './modules/TodoList'; 
+// import { useEffect } from 'react';
 
 
-function App() {
-  const [tasks, setTasks] = useState(()=>{
-    const storedTasks = localStorage.getItem('tasks');
-    if(storedTasks){
-      return JSON.parse(storedTasks);
-    }
-    return[];
-  }
-  );
-  const [selectedCategory, setSelectedCategory] = useState(null);
+// function App() {
+//   const [tasks, setTasks] = useState(()=>{
+//     const storedTasks = localStorage.getItem('tasks');
+//     if(storedTasks){
+//       return JSON.parse(storedTasks);
+//     }
+//     return[];
+//   }
+//   );
+//   const [selectedCategory, setSelectedCategory] = useState(null);
 
-  useEffect(()=>{
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks]);
+//   useEffect(()=>{
+//     localStorage.setItem('tasks', JSON.stringify(tasks));
+//   }, [tasks]);
 
   
   
   
-  const addTask = (taskText) => {
-    const newTask = {
-      id: Date.now(), 
-      text: taskText,
-      completed: false,
-    };
-    setTasks([...tasks, newTask]);
-  };
+//   const addTask = (taskText) => {
+//     const newTask = {
+//       id: Date.now(), 
+//       text: taskText,
+//       completed: false,
+//     };
+//     setTasks([...tasks, newTask]);
+//   };
 
 
-  const deleteTask = (taskId) => {
-    setTasks(tasks.filter(task => task.id !== taskId));
-  };
+//   const deleteTask = (taskId) => {
+//     setTasks(tasks.filter(task => task.id !== taskId));
+//   };
 
-  const toggleTask = (taskId) => {
-    setTasks(tasks.map(task => 
-      task.id === taskId ? { ...task, completed: !task.completed } : task
-    ));
-  };
+//   const toggleTask = (taskId) => {
+//     setTasks(tasks.map(task => 
+//       task.id === taskId ? { ...task, completed: !task.completed } : task
+//     ));
+//   };
 
-  const editTask = (taskId, newName) => {
-    setTasks(tasks.map(task=>
-      task.id === taskId ? {...task, text: newName} : task
-    ));
-  };
-
-
-  const filteredTasks = tasks.filter(task =>{
-    if(selectedCategory === 'active'){
-      return !task.completed;
-    }
-    if(selectedCategory === 'completed'){
-      return task.completed;
-    }
-    return true; //all이면 그냥 할일 다 보여줌
-  })
+//   const editTask = (taskId, newName) => {
+//     setTasks(tasks.map(task=>
+//       task.id === taskId ? {...task, text: newName} : task
+//     ));
+//   };
 
 
+//   const filteredTasks = tasks.filter(task =>{
+//     if(selectedCategory === 'active'){
+//       return !task.completed;
+//     }
+//     if(selectedCategory === 'completed'){
+//       return task.completed;
+//     }
+//     return true; //all이면 그냥 할일 다 보여줌
+//   })
 
 
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center font-sans">
-      <div className="w-full max-w-lg bg-white shadow-lg rounded-lg p-8">
-        <Header />
-        <AddTodo onAddTask={addTask} />
-        <Category 
-          selected={selectedCategory} 
-          onCategoryChange={setSelectedCategory} />
-        <TodoList 
-          tasks={filteredTasks} 
-          onToggle={toggleTask} 
-          onDelete={deleteTask} 
-          onEdit={editTask}
-          />
-      </div>
-    </div>
-  );
-}
 
-export default App;
 
-// import LoginPage from "./pages/LoginPage";
-// import './App.css'
-
-// function App(){
-//   return(
-//   <LoginPage/>
+//   return (
+//     <div className="min-h-screen bg-gray-100 flex items-center justify-center font-sans">
+//       <div className="w-full max-w-lg bg-white shadow-lg rounded-lg p-8">
+//         <Header />
+//         <AddTodo onAddTask={addTask} />
+//         <Category 
+//           selected={selectedCategory} 
+//           onCategoryChange={setSelectedCategory} />
+//         <TodoList 
+//           tasks={filteredTasks} 
+//           onToggle={toggleTask} 
+//           onDelete={deleteTask} 
+//           onEdit={editTask}
+//           />
+//       </div>
+//     </div>
 //   );
 // }
+
 // export default App;
+
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import './App.css'
+
+function App(){
+  return(
+  <SignupPage/>
+  );
+}
+export default App;
 
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,74 @@
+import {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/button";
+import Input from "../components/Input";
+
+function SignupPage(){
+    const [name, setName] = useState('');
+    const [id, setId] = useState('');
+    const [pwd, setPwd] = useState('');
+    
+    const navigate = useNavigate();
+
+    const handleSignupPage = (e) =>{
+        e.preventDefault();
+        console.log('회원가입시도');
+    };
+
+    const navToLogin = ()=>{
+        navigate('/Login');
+    };
+
+    return(
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+                <h2 className="text-2xl font-bold text-center">
+                    회원가입
+                </h2>
+                <form className="space-y-4" onSubmit={handleSignupPage}>
+                    <div>
+                        <label htmlFor="name" className="block mb-1 text-sm font-medium text-gray-700">
+                            이름
+                        </label>
+                        <Input
+                            id="name"
+                            name="named"
+                            placeholder="이름을 입력하세요"
+                            value={name}
+                            onChange={(e)=>setName(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="id" className="block mb-1 text-sm font-medium text-gray-700">
+                            아이디
+                        </label>
+                        <Input
+                            id="id"
+                            name="id"
+                            placeholder="아이디를 입력하세요"
+                            value={id}
+                            onChange={(e)=>setId(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="pwd" className="block mb-1 test-sm font-medium text-gray-700">
+                            비밀번호
+                        </label>
+                        <Input 
+                            id="pwd"
+                            name="pwd"
+                            type="password"
+                            placeholder="비밀번호 입력하세요"
+                            value={pwd}
+                            onChange={(e)=>setPwd(e.target.vlaue)}/>
+                    </div>
+                    <div>
+                        <Button type="submit">
+                            회원가입
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default SignupPage;
