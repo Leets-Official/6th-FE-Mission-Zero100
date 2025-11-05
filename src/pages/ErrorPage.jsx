c99a163fa8084f575346c7545691e6f4ef3bb20a
YeBeenChoi <yebin607@naver.com>
2025-11-05 16:48:01 +0900

    fix: ErrorPage html 태그 오타 수정

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,7 +4,7 @@ export default function ErrorPage() {
   return (
     <div className='min-h-screen flex flex-col items-center justify-center bg-red-50 text-red-700'>
       <h1 className='text-3xl font-bold mb-4'>404 Not Found</h1>
-      <o>페이지를 찾을 수 없습니다.</o>
+      <p>페이지를 찾을 수 없습니다.</p>
       <Link to='/' className='mt-6 underline'>
         홈으로 돌아가기
       </Link>
