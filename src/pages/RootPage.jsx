ec63c17beb59d3e8f4db038eb2bbd284b9904872
Wonjun Jung <jwj0620@gachon.ac.kr>
2025-11-25 02:25:22 +0900

    feat: RootPage.jsx 카카오 로그인 ui 버튼 추가 및 백엔드 이동 함수 추가

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -3,31 +3,46 @@ import Text from "@/components/Text";
 import Button from "@/components/Button";
 
 export default function RootPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="flex flex-col items-center justify-center h-screen bg-white text-center">
-            {/* 제목 */}
-            <Text as="h1" className="text-3xl font-bold mb-10 text-black">
-                OO’s TODO
-            </Text>
+  const kakaoLogin = () => {
+    window.location.href = "http://localhost:3001/auth/kakao";
+  };
 
-            {/* 버튼 영역 */}
-            <div className="flex flex-col gap-4 w-48">
-                <Button
-                    className="w-full py-3 rounded-full bg-gray-200 text-black text-lg font-bold hover:bg-gray-300 transition"
-                    onClick={() => navigate("/login")}
-                >
-                    로그인
-                </Button>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white text-center">
+      <Text as="h1" className="text-3xl font-bold mb-10 text-black">
+        OO’s TODO
+      </Text>
 
-                <Button
-                    className="w-full py-3 rounded-full bg-gray-200 text-black text-lg font-bold hover:bg-gray-300 transition"
-                    onClick={() => navigate("/signup")}
-                >
-                    회원가입
-                </Button>
-            </div>
-        </div>
-    );
+      <div className="flex flex-col gap-4 w-56">
+
+        {/* 일반 로그인 버튼 */}
+        <Button
+          className="w-full py-3 rounded-full bg-gray-200 text-black text-lg font-bold hover:bg-gray-300 transition"
+          onClick={() => navigate("/login")}
+        >
+          로그인
+        </Button>
+
+        {/* 일반 회원가입 버튼 */}
+        <Button
+          className="w-full py-3 rounded-full bg-gray-200 text-black text-lg font-bold hover:bg-gray-300 transition"
+          onClick={() => navigate("/signup")}
+        >
+          회원가입
+        </Button>
+
+        {/*  카카오 로그인 버튼 */}
+        <button onClick={kakaoLogin} className="mt-2">
+          <img
+            src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+            width="222"
+            alt="카카오 로그인 버튼"
+            className="mx-auto"
+          />
+        </button>
+      </div>
+    </div>
+  );
 }
