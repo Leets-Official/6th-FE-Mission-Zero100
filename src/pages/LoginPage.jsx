9875447ffa82b0325c841bc9c234e02102b17494
baeys0619 <baeys2005@gmail.com>
2025-11-30 03:20:09 +0900

    fix : LoginPage에서의 import 대소문자 구분 수정

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import Button from "../components/button";
+import Button from "../components/Button";
 import Input from "../components/Input";
 import KakaoIcon from '../assets/kakao.svg?react';
 
