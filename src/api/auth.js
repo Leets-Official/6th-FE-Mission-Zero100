247d2a76dd6471c07fb0e8747328667f6578a927
Wonjun Jung <jwj0620@gachon.ac.kr>
2025-12-02 15:02:03 +0900

    feat: axios 중복 생성 대신 api(인스턴스) 를 import

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,18 +1,12 @@
-import axios from "axios";
-
-// 백엔드 서버 요청용 axios 인스턴스
-export const api = axios.create({
-  baseURL: "https://blog.leets.land",
-  withCredentials: true,
-  timeout: 5000,
-});
+// api.js에서 만든 axios 인스턴스 불러오기
+import { api } from "./api";
 
 // 로컬 json-server 주소
 const LOCAL_USER_URL = "http://localhost:3000/users";
 
 // 로컬 json-server: 이메일로 사용자 조회
 export const fetchUserByEmail = async (email) => {
-  const res = await axios.get(LOCAL_USER_URL, {
+  const res = await api.get(LOCAL_USER_URL, {
     params: { email },
   });
   return res.data;
@@ -20,22 +14,22 @@ export const fetchUserByEmail = async (email) => {
 
 // 로컬 json-server: 일반 회원가입
 export const signupUser = async (userData) => {
-  const res = await axios.post(LOCAL_USER_URL, userData);
+  const res = await api.post(LOCAL_USER_URL, userData);
   return res.data;
 };
 
 // 로컬 json-server: 일반 로그인
 export const loginUser = async ({ email, password }) => {
-  const res = await fetchUserByEmail(email);
-  if (res.length === 0) throw new Error("존재하지 않는 아이디입니다.");
+  const users = await fetchUserByEmail(email);
+  if (users.length === 0) throw new Error("존재하지 않는 아이디입니다.");
 
-  const user = res[0];
+  const user = users[0];
   if (user.password !== password) throw new Error("비밀번호가 일치하지 않습니다.");
 
   return user;
 };
 
-// OAuth 회원가입 (swagger 스펙에 맞게 요청)
+// OAuth 회원가입
 export const registerOauth = async ({
                                       email,
                                       nickname,
@@ -59,10 +53,11 @@ export const registerOauth = async ({
   return data;
 };
 
-// 카카오 로그인 redirect 처리
+// 카카오 redirect 처리
 export const kakaoLogin = async (code) => {
   const { data } = await api.get("/auth/kakao/redirect", {
     params: { code },
   });
   return data;
 };
+g
